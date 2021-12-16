9bd14a5efc2a07f11a77c4efee0783271f1a090b
Artem Gavryushenko <artyomgavryeshenko@gmail.com>
2021-12-16 15:29:41 +0200

    improved the localization

--- frontend/src/components/cards/results-card/results-card.js
+++ frontend/src/components/cards/results-card/results-card.js
@@ -31,11 +31,11 @@ const Results = () => {
         let mentalComment = "";
       let weightComment = "";
         console.log(data);
-         data.weightIsOk ? weightComment = t("Your weight looks good. ") : weightComment = t("Obesity might cause productivy loss and depression. ");
-        (data.hr > 99 || data.hr < 59) ? heartRateComment=t("Heart rate over normal might be caused by stress. "):
-        heartRateComment = t("Your cardiovascular system looks okay. ");
-        (data.mentalInstruction) ? mentalComment =t("We strongly advise a visit to a therapist. "):
-        mentalComment =t("Your mental health is good. ");
+         data.weightIsOk ? weightComment = "Your weight looks good. " : weightComment = "Obesity might cause productivy loss and depression. ";
+        (data.hr > 99 || data.hr < 59) ? heartRateComment=("Heart rate over normal might be caused by stress. "):
+        heartRateComment = "Your cardiovascular system looks okay. ";
+        (data.mentalInstruction) ? mentalComment =("We strongly advise a visit to a therapist. "):
+        mentalComment =("Your mental health is good. ");
         const arr =[];
         arr.push(weightComment, heartRateComment, mentalComment);
         console.log(arr)
@@ -52,22 +52,19 @@ const Results = () => {
     doc.setFont("times",  "bold")
     doc.text(250, 20, 'My results')
     doc.setFont("times", "italic", "normal")
-    doc.text(20, 60, `saturation: ${data.saturation}`)
-    doc.text(20, 90, `${commments[0]}`)
-    doc.text(20, 120, `${commments[1]}`)
-    doc.text(20, 150, `Bllod results:`)
-    doc.text(20, 180, `Leukocytes: ${data.leuko}`)
-    doc.text(250, 180, `Lymphocytes: ${data.limfo}`)
-    doc.text(20, 210, `Bazophils: ${data.bezof}`)
-    doc.text(250, 210, `Erythrocytes: ${data.eryth}`)
-    doc.text(20, 240, `Eosinophils: ${data.ezino}`)
-    doc.text(250, 240, `Hemoglobin: ${data.hemo}`)
-    doc.text(20, 270, `Monocytes: ${data.mono}`)
-    doc.text(250, 270, `Patelets: ${data.trombo}`)
-    doc.text(20, 310, `${commments[2]}`)
-    doc.text(20, 340, `Heart rate ${data.heartRate}`)
+    doc.text(20, 90, ` ${commments[0]}`)
+    doc.text(20, 120, ` ${commments[1]}`)
+
+    doc.text(20, 150, `Mental results:`)
+    let text = ("Stress") + "  " + data.stress_p;
+    doc.text(20, 180,  text)
+    doc.text(250, 180, `General: ${data.general_p}`)
+    doc.text(20, 210, `Happiness: ${data.happiness_p}`)
+    doc.text(250, 210, `Depression: ${data.depression_p}`)
+    doc.text(20, 240, `Anxiety: ${data.anxiety_p}`)
+
     doc.text(20, 370, `Psychological check: `)
-    doc.text(200, 370, `${commments[3]}`)
+    doc.text(200, 370, `${commments[2]}`)
     doc.text(360, 420, `${date}`)
     doc.addImage(logo, "PNG", 360, 450, 180, 180)
 
@@ -126,22 +123,22 @@ const Results = () => {
           </Grid>
           <Grid item xs={12} sm={12}>
             <Typography variant="subtitle1" color="textSecondary">
-              {t("Recommendations")}: <strong>{commments[0] + " " + commments[1] + " " + commments[2]}</strong>
+              {t("Recommendations")}: <strong>{t(commments[0]) + " " + t(commments[1]) + " " + t(commments[2])}</strong>
             </Typography>
           </Grid>
 
-          <Grid item sm={12}>
-          <Typography variant="subtitle1" color="textSecondary">
-            {t("To download your recommendations press the Download button")}
-          </Typography>
-        </Grid>
+        {/*  <Grid item sm={12}>*/}
+        {/*  <Typography variant="subtitle1" color="textSecondary">*/}
+        {/*    {t("To download your recommendations press the Download button")}*/}
+        {/*  </Typography>*/}
+        {/*</Grid>*/}
           <Grid className container alignItems="flex-start" spacing={1}>
 
-            <Grid item sm={12}>
-              <Button  className={classes.button} onClick={generatePDF} >
-                {t("Download")}
-              </Button>
-            </Grid>
+            {/*<Grid item sm={12}>*/}
+            {/*  <Button  className={classes.button} onClick={generatePDF} >*/}
+            {/*    {t("Download")}*/}
+            {/*  </Button>*/}
+            {/*</Grid>*/}
           </Grid>
         </Grid>
         </Card>
